03818e5b109a4d6f556de995346c418b5e9763e1
Jarax73 <jaraxkapila73@gmail.com>
2025-02-14 22:13:37 +0100

    working on it

--- app/about/page.js
+++ app/about/page.js
@@ -9,26 +9,7 @@ export default function Page() {
   const prompt = () => alert("Working on it...");
   return (
     <div className={styles.about}>
-      <div className={styles.contact_container}>
-        <div className={styles.image_container}>
-          <Image
-            src="/images/contact.png"
-            alt="contact"
-            width={400}
-            className={styles.image}
-            height={550}
-          />
-          <span className={styles.tooltiptext}>
-            📩 Envie d’échanger sur le développement, un projet ou l’innovation
-            ?
-          </span>
-        </div>
-        <button className={styles.button} onClick={prompt}>
-          Voir mon CV
-        </button>
-      </div>
-
-      <CardCarousel />
+      <p>Working on it...</p>
     </div>
   );
 }
