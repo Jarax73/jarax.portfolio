a9a0bb2a06f7331713541393d3856814950de6ca
Jarax73 <jaraxkapila73@gmail.com>
2025-02-26 13:59:14 +0100

    Add not found page

---
+++ app/not-found.js
@@ -0,0 +1,15 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontWeight: "bold",
+      }}
+    >
+      <h1>😢 404 - Page Not Found</h1>
+    </div>
+  );
+}
---
+++ app/tools/loading.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Loading() {
+  <div>
+    <Image src="/jared.png" alt="" style={{ boxShadow: "0 0 10px #cecece" }} />
+    <h1>Loading...</h1>
+  </div>;
+}
